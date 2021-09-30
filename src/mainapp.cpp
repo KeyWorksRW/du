03ca0a306aff676e3aa8eddb77b1130ad6f8ff9f
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2021-09-30 16:50:38 -0700

    Create CMake project files

--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -14,6 +14,20 @@
 #include <ttparser.h>   // cmd -- Command line parser
 #include <ttwinff.h>    // Wrapper around Windows FindFile
 
+#if defined(_WIN32)
+
+    #pragma comment(lib, "kernel32.lib")
+    #pragma comment(lib, "user32.lib")
+    #pragma comment(lib, "gdi32.lib")
+    #pragma comment(lib, "comctl32.lib")
+    #pragma comment(lib, "comdlg32.lib")
+    #pragma comment(lib, "shell32.lib")
+
+    #pragma comment(lib, "rpcrt4.lib")
+    #pragma comment(lib, "advapi32.lib")
+
+#endif
+
 #include "strings.h"  // Contains translatable string IDs
 
 struct FILE_SIZES
